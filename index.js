25a97683c37f44f51870c5f91246db7b4195d58f
superastorh <salem.m.s2030@gmail.com>
2021-03-19 10:23:42 +0300

    use prettier and add back in about

--- index.js
+++ index.js
@@ -1,131 +1,170 @@
 "use strict";
 
-require('dotenv').config()
+require("dotenv").config();
 
 String.prototype.replaceAll = function replaceAll(search, replace) {
-    return this.split(search).join(replace);
-}
+  return this.split(search).join(replace);
+};
 
-const licenseUrl = "https://ojuba.org/waqf-2.0:%D8%B1%D8%AE%D8%B5%D8%A9_%D9%88%D9%82%D9%81_%D8%A7%D9%84%D8%B9%D8%A7%D9%85%D8%A9";
-let {eat, drink, word} = require('./src/lists');
+const licenseUrl =
+  "https://ojuba.org/waqf-2.0:%D8%B1%D8%AE%D8%B5%D8%A9_%D9%88%D9%82%D9%81_%D8%A7%D9%84%D8%B9%D8%A7%D9%85%D8%A9";
+let { eat, drink, word } = require("./src/lists");
 
 const adminID = "635096382";
 
-const {Telegraf, Markup} = require("telegraf");
+const { Telegraf, Markup } = require("telegraf");
 
-const {getApi, checker, drinkReplay, eatReplay, replace, replayId, Supporter} = require("./src/lib");
+const {
+  getApi,
+  checker,
+  drinkReplay,
+  eatReplay,
+  replace,
+  replayId,
+  Supporter,
+} = require("./src/lib");
 
 const bot = new Telegraf(process.env.BOT_TOKEN || getApi());
-
-const buttons = Markup.inlineKeyboard([
-    [
-        Markup.button.url('المطور', 'https://t.me/superastorh'),
-        Markup.button.url("الرخصة", licenseUrl)
-    ],
-    [
-        Markup.button.callback("ادعمنا", "supportMe"),
-        Markup.button.callback("الداعمين", "Supporter")
-    ],
-    [Markup.button.callback("قائمة عبود", "menu")],
-])
-
-bot.launch().then(() => start());
-
-bot.command("about", ctx => {
-    if (ctx.message.chat.type !== 'supergroup') {
-        ctx.reply(`بوت عبود هو بوت للتسلية داخل المجموعات 
+const about = `بوت عبود هو بوت للتسلية داخل المجموعات 
 البوت مجاني تماما و مفتوح المصدر
 
 لذالك نروج منك دعمنا حتى نستمر ب تجربة بوت خالي تماما من الاعلانات
-    `, buttons);
-    } else {
-        ctx.reply("لاتعمل الرساله في المجموعات تواصل معي خاص" + " @" + bot.botInfo.username)
+`;
+const back = Markup.inlineKeyboard([Markup.button.callback("رجوع", "about")]);
+const buttons = Markup.inlineKeyboard([
+  [
+    Markup.button.url("المطور", "https://t.me/superastorh"),
+    Markup.button.url("الرخصة", licenseUrl),
+  ],
+  [
+    Markup.button.callback("ادعمنا", "supportMe"),
+    Markup.button.callback("الداعمين", "Supporter"),
+  ],
+  [Markup.button.callback("بوتات اخرى من صنعنا", "myBots")],
+  [Markup.button.callback("قائمة عبود", "menu")],
+]);
+
+bot.launch().then(() => start());
+
+bot.command("about", (ctx) => {
+  if (ctx.message.chat.type !== "supergroup") {
+    ctx.reply(about, buttons);
+  } else {
+    ctx.reply(
+      "لاتعمل الرساله في المجموعات تواصل معي خاص" + " @" + bot.botInfo.username
+    );
+  }
+});
+bot.action("myBots", (ctx) => {
+  let keyBord = Markup.inlineKeyboard([
+    [Markup.button.url("بوت عبود للشاي", "https://t.me/artea_bot")],
+    [
+      Markup.button.callback("ادعمنا", "supportMe"),
+      Markup.button.callback("رجوع", "about"),
+    ],
+  ]);
+  action(
+    ctx,
+    "البوتات الاخرى التي تم صنعناها وهي من تطوير @superastorh",
+    keyBord
+  );
+});
+
+bot.action("Supporter", (ctx) => {
+  let keyBord = Markup.inlineKeyboard([
+    [
+      Markup.button.callback("ادعمنا", "supportMe"),
+      Markup.button.callback("رجوع", "about"),
+    ],
+  ]);
+  action(
+    ctx,
+    "الداعمين هم السبب الرائيسي في عمل البوت الخاص بنا وهم" +
+      "\n\n" +
+      Supporter(),
+    keyBord
+  );
+});
+
+bot.action("menu", (ctx) => {
+  let { to } = require("./src/lists").word;
+  action(ctx, to[2], back);
+});
+
+bot.action("supportMe", (ctx) => {
+  action(
+    ctx,
+    "اذا كنت ترغب بدعمنا نرجو منك التواصل مع مطور البوت لمعرفة التفاضيل الازمة \n مطور البوت : @superastorh",
+    back
+  );
+});
+bot.action("about", (ctx) => {
+  action(ctx, about, buttons);
+});
+
+bot.hears(["عبود", "ياعبود", "عبودي", "ياعبودي"], (ctx) =>
+  ctx.reply("معك معلم")
+);
+
+bot.hears("تسلم", (ctx) => {
+  if (
+    ctx.message.reply_to_message &&
+    ctx.message.reply_to_message.from.username === bot.botInfo.username
+  ) {
+    replayId(ctx, "لا شكر على واجب");
+  }
+});
+
+bot.on("text", (ctx) => {
+  let txt = ctx.message.text;
+
+  txt = replace(txt);
+
+  let wordIndex = word.from.indexOf(txt);
+
+  if (-1 !== wordIndex) {
+    replayId(ctx, word.to[wordIndex]);
+  } else if (checker(txt)) {
+    txt = txt.replace("عبود", "");
+
+    let replay;
+
+    if (-1 !== drink.ls1.indexOf(txt)) {
+      replay = drinkReplay(drink.ls1.indexOf(txt));
+
+      replayId(ctx, replay);
+    } else if (-1 !== eat.ls1.indexOf(txt)) {
+      replay = eatReplay(eat.ls1.indexOf(txt));
+
+      replayId(ctx, replay);
     }
-})
-
-bot.action("Supporter", ctx => {
-    let keyBord = Markup.inlineKeyboard([
-        [Markup.button.callback("ادعمنا", "supportMe")]
-    ]);
-    action(ctx, "الداعمين هم السبب الرائيسي في عمل البوت الخاص بنا وهم" + "\n\n" + Supporter(), keyBord)
-})
-
-bot.action("menu", ctx => {
-    let {to} = require("./src/lists").word;
-    action(ctx, to[2])
-})
-
-bot.action("supportMe", ctx => {
-    action(ctx, "اذا كنت ترغب بدعمنا نرجو منك التواصل مع مطور البوت لمعرفة التفاضيل الازمة \n مطور البوت : @superastorh")
-})
-
-bot.hears(['عبود', 'ياعبود', "عبودي", "ياعبودي"], (ctx) => ctx.reply('معك معلم'))
-
-bot.hears('تسلم', (ctx) => {
-    if (ctx.message.reply_to_message && ctx.message.reply_to_message.from.username === bot.botInfo.username) {
-        replayId(ctx, "لا شكر على واجب")
-    }
-})
-
-bot.on('text', (ctx) => {
-
-    let txt = ctx.message.text
-
-    txt = replace(txt);
-
-    let wordIndex = word.from.indexOf(txt);
-
-
-    if (-1 !== wordIndex) {
-        replayId(ctx, word.to[wordIndex])
-
-    } else if (checker(txt)) {
-        txt = txt.replace("عبود", "");
-
-        let replay;
-
-        if (-1 !== drink.ls1.indexOf(txt)) {
-
-            replay = drinkReplay(drink.ls1.indexOf(txt));
-
-            replayId(ctx, replay);
-
-        } else if (-1 !== eat.ls1.indexOf(txt)) {
-
-            replay = eatReplay(eat.ls1.indexOf(txt));
-
-            replayId(ctx, replay);
-
-        }
-
-    }
-})
+  }
+});
 
 function start() {
-    sendMessage(adminID, "اشتغل بوت" + "\n @" + bot.botInfo.username, {});
+  sendMessage(adminID, "اشتغل بوت" + "\n @" + bot.botInfo.username, {});
 }
 
 function stop(stop) {
-    if (stop) bot.stop(stop).then();
-    sendMessage(adminID, "تقفل بوت" + "\n @" + bot.botInfo.username);
+  if (stop) bot.stop(stop).then();
+  sendMessage(adminID, "تقفل بوت" + "\n @" + bot.botInfo.username);
 }
 
-process.once('SIGINT', () => stop('SIGINT'));
+process.once("SIGINT", () => stop("SIGINT"));
 
-process.once('SIGTERM', () => stop('SIGTERM'));
+process.once("SIGTERM", () => stop("SIGTERM"));
 
 function deleteMessage(chat_id, message_id) {
-    bot.telegram.deleteMessage(chat_id, message_id).then()
+  bot.telegram.deleteMessage(chat_id, message_id).then();
 }
 
-function sendMessage(chatId , text , extra = {} ) {
-    bot.telegram.sendMessage(chatId, text, extra).then()
+function sendMessage(chatId, text, extra = {}) {
+  bot.telegram.sendMessage(chatId, text, extra).then();
 }
 
-
-function action(ctx , message , extra = {}){
-    let chat = ctx.update.callback_query.message.chat.id;
-    let messageId = ctx.update.callback_query.message.message_id;
-    deleteMessage(chat, messageId)
-    sendMessage(chat, message , extra)
+function action(ctx, message, extra = {}) {
+  let chat = ctx.update.callback_query.message.chat.id;
+  let messageId = ctx.update.callback_query.message.message_id;
+  deleteMessage(chat, messageId);
+  sendMessage(chat, message, extra);
 }
--- src/lib.js
+++ src/lib.js
@@ -1,81 +1,83 @@
 module.exports = {
-    //  التحقق هل الرساله للبوت ام لا
+  //  التحقق هل الرساله للبوت ام لا
 
-    checker(input) {
-        return input.startsWith("عبود") || input.endsWith("عبود")
-    },
+  checker(input) {
+    return input.startsWith("عبود") || input.endsWith("عبود");
+  },
 
-    // استبدال لكي يعمل
+  // استبدال لكي يعمل
 
-    replace(input) {
-        let x = input.toString();
-        x = x.replaceAll("ياعبود", "عبود")
-        x = x.replaceAll("عبودي", "عبود")
-        x = x.replaceAll("ياعبودي", "عبود")
-        x = x.replaceAll(" ", "")
-        x = x.replaceAll("/n", "")
-        x = x.replaceAll("ة", "ه")
-        x = x.replaceAll("أ", "ا")
-        x = x.replaceAll("القهوه", "قهوه")
-        x = x.replaceAll("الشاي", "شاي")
-        x = x.replaceAll("البرتقال", "برتقال")
-        x = x.replaceAll("اليمون", "ليمون")
-        x = x.replaceAll("ماكولات", "اكلات")
-        x = x.replaceAll("كولا", "كوكولا")
-        x = x.replaceAll("شورما", "شاورما")
-        x = x.replaceAll("ليموناضه", "عصيرليمون")
-        return x;
-    },
+  replace(input) {
+    let x = input.toString();
+    x = x.replaceAll("ياعبود", "عبود");
+    x = x.replaceAll("عبودي", "عبود");
+    x = x.replaceAll("ياعبودي", "عبود");
+    x = x.replaceAll(" ", "");
+    x = x.replaceAll("/n", "");
+    x = x.replaceAll("ة", "ه");
+    x = x.replaceAll("أ", "ا");
+    x = x.replaceAll("القهوه", "قهوه");
+    x = x.replaceAll("الشاي", "شاي");
+    x = x.replaceAll("البرتقال", "برتقال");
+    x = x.replaceAll("اليمون", "ليمون");
+    x = x.replaceAll("ماكولات", "اكلات");
+    x = x.replaceAll("كولا", "كوكولا");
+    x = x.replaceAll("شورما", "شاورما");
+    x = x.replaceAll("ليموناضه", "عصيرليمون");
+    return x;
+  },
 
-    eatReplay(index) {
-        let eat = require('./lists').eat;
+  eatReplay(index) {
+    let eat = require("./lists").eat;
 
-        let {random} = require('./lib');
+    let { random } = require("./lib");
 
-        return random(eat.ls4).replaceAll("%3", eat.ls3[index]) + eat.ls2[index]
-    },
+    return random(eat.ls4).replaceAll("%3", eat.ls3[index]) + eat.ls2[index];
+  },
 
-    drinkReplay(index) {
-        let drink = require('./lists').drink;
+  drinkReplay(index) {
+    let drink = require("./lists").drink;
 
-        let {random} = require('./lib');
+    let { random } = require("./lib");
 
-        return random(drink.ls4).replaceAll("%3", drink.ls3[index]) + drink.ls2[index]
-    },
+    return (
+      random(drink.ls4).replaceAll("%3", drink.ls3[index]) + drink.ls2[index]
+    );
+  },
 
-    random(array = []) {
-        return array[Math.floor(Math.random() * array.length)]
-    },
+  random(array = []) {
+    return array[Math.floor(Math.random() * array.length)];
+  },
 
-    getApi() {
+  getApi() {
+    const prompt = require("prompt-sync")();
 
-        const prompt = require('prompt-sync')();
+    const fs = require("fs");
 
-        const fs = require('fs');
+    const api = prompt("What is your api bot? => ");
 
-        const api = prompt('What is your api bot? => ');
+    const content = "BOT_TOKEN=" + api;
 
-        const content = 'BOT_TOKEN=' + api;
+    fs.writeFile("./.env", content, (err) => {});
 
-        fs.writeFile('./.env', content, err => {
-        });
+    return api;
+  },
 
-        return api;
-
-    },
-
-    replayId(ctx, txt, extra = {"reply_to_message_id": ctx.update.message.message_id}) {
-        ctx.reply(txt, extra);
-    },
-    Supporter() {
-        const supporter = {
-            "Elmatador91": "المُثْنىَ الدِمَشْقِي",
-        }
-        let text = "";
-        for (let support in supporter) {
-            text +=supporter[support] + " : @" + support + "\n"
-        }
-        return text
+  replayId(
+    ctx,
+    txt,
+    extra = { reply_to_message_id: ctx.update.message.message_id }
+  ) {
+    ctx.reply(txt, extra);
+  },
+  Supporter() {
+    const supporter = {
+      Elmatador91: "المُثْنىَ الدِمَشْقِي",
+    };
+    let text = "";
+    for (let support in supporter) {
+      text += supporter[support] + " : @" + support + "\n";
     }
-
-}
+    return text;
+  },
+};
